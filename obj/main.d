264f7acf9d3825f6b9f7f572817c797f02747161
ZardyFenn <fabioangelonizardy@gmail.com>
2025-11-28 10:29:04 -0300

    penultima

---
+++ obj/main.d
@@ -0,0 +1,10 @@
+obj/main.o: main.c include/types.h include/funcao_base.h \
+ include/./types.h include/aeronaves_lista.h include/remocoes.h \
+ include/filtros.h include/consulta_rotas.h
+include/types.h:
+include/funcao_base.h:
+include/./types.h:
+include/aeronaves_lista.h:
+include/remocoes.h:
+include/filtros.h:
+include/consulta_rotas.h:
